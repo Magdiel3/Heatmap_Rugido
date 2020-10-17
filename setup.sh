7c7fef5eba79975d84aa16609072bd8fc46d0847
Magdiel3 <magdieltercero@hotmail.com>
2020-10-17 11:31:33 -0500

    Setup Files

--- setup.sh
+++ setup.sh
@@ -12,8 +12,8 @@ enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
 
-mkdir data_store
+# mkdir data_store
 
-python spreadsheet.py
+# python spreadsheet.py
 
-python update_addresses.py
+# python update_addresses.py
